a1ae72920461fd9336aa71db97c4c8f78bd62720
Dave Paroulek <dparoulek@gmail.com>
2010-09-02 18:13:57 -0400

    working on capistrano recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'mongrel_cluster/recipes'
+require 'lib/recipes/capistrano_database'
 
 ssh_options[:port] = 20001
 default_run_options[:pty] = true
@@ -7,25 +8,17 @@ set :scm, "git"
 set :repository,  "git@github.com:dparoulek/myblog.git"
 set :user, "dparoulek"
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
 set :branch, "master"
 set :deploy_via, :remote_cache
+set :deploy_to, "/var/rails/myblog"
 
-set :deploy_to, "/home/myblogadmin"
-
-role :web, "jackets"                          # Your HTTP server, Apache/etc
+role :web, "hokies"                          # Your HTTP server, Apache/etc
 role :app, "jackets"                          # This may be the same as your `Web` server
 role :db,  "jackets", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-# ensure this is set after :deploy_to
-# set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+task :move_static_files, :roles => :web do
+  desc "Starting moving the static public files to Apache Document Root."
+  upload("/var/rails/#{application}/public", "/home/myblogadmin/", :via => :scp, :recursive => true)
+  desc "Ending moving the static public files to Apache Document Root."
+end
